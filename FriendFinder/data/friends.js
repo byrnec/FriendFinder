a7312c9b8ac68338945adb503b5e5e7eeb6dd385
CB <byrne.christie@gmail.com>
2018-12-20 10:11:25 -0800

    set up routes, add all survey questions

---
+++ FriendFinder/data/friends.js
@@ -0,0 +1,110 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Travis Price",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "5",
+      "2",
+      "4",
+      "4",
+      "5",
+      "3",
+      "2",
+      "3",
+      "4",
+      "3"
+    ]
+  },
+  {
+    name: "Tyler Moragne",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "4",
+      "2",
+      "2",
+      "1",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Beka Aziz",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Brett Byrne",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Everett Lynn",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "5",
+      "5",
+      "3",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Colleen Mahoney",
+    photo: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/48020604_1950313331940243_6851546544584261632_n.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=459abb9ca4e21a922317970743909dfc&oe=5C9C0685",
+    scores: [
+      "4",
+      "5",
+      "2",
+      "5",
+      "2",
+      "2",
+      "4",
+      "2",
+      "1",
+      "1"
+    ]
+  }
+
+];
+
+// export the array
+module.exports = friends;
--- FriendFinder/routes/apiRoutes.js
+++ FriendFinder/routes/apiRoutes.js
@@ -1,63 +1,69 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var friends = require('../data/friends.js');
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+module.exports = function(app){
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get('/api/friends', function(req, res){
+    res.json(friends);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+  // user submits a form and it submits data to the server.
+  app.post('/api/friends', function(req, res){
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
+  // Note the code here. Our "server" will respond to a user"s survey result
+    // Then compare those results against every user in the database.
+    // It will then calculate the difference between each of the numbers and the user"s numbers.
+    // It will then choose the user with the least differences as the "best friend match."
+    // In the case of multiple users with the same result it will choose the first match.
+    // After the test, it will push the user to the database.
+
+    // We will use this object to hold the "best match". We will constantly update it as we
+    // loop through all of the options
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    // Here we take the result of the user"s survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    // This variable will calculate the difference between the user"s scores and the scores of
+    // each user in the database
+    var totalDifference = 0;
+
+    // Here we loop through all the friend possibilities in the database.
+    for (var i = 0; i < friends.length; i++) {
+
+      console.log(friends[i].name);
+      totalDifference = 0;
+
+      // We then loop through all the scores of each friend
+      for (var j = 0; j < friends[i].scores[j]; j++) {
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = friends[i].name;
+          bestMatch.photo = friends[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+
+    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+    // the database will always return that the user is the user's best friend).
+    friends.push(userData);
+
+    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+    res.json(bestMatch);
+
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    tableData.length = [];
-    waitListData.length = [];
-
-    res.json({ ok: true });
-  });
 };
